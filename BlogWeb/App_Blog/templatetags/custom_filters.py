ff6c63ffc43f5d5f66a9d1d4bcc253936edc9528
rahat-4 <mainul.rahat4@gmail.com>
2022-07-13 01:12:26 +0600

    first commit

--- BlogWeb/App_Blog/templatetags/custom_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-def short_content(value):
-    return value[:500] + ' .....'
-
-register.filter('short_content', short_content)
--- BlogWeb/BlogWeb/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import HttpResponseRedirect
-from django.urls import reverse
-
-def index(request):
-    return HttpResponseRedirect(reverse('App_Blog:blog_list'))
